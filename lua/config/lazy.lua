ec8fe8e364954429911db8d6c841c857a41a9a47
waffle6532 <github+work@blaisehuwiler.ch>
2025-02-12 08:56:47 +0100

    feat: removed unnecessary packages

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -44,9 +44,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.lsp.none-ls" },
     { import = "lazyvim.plugins.extras.test.core" },
-    { import = "lazyvim.plugins.extras.ui.smear-cursor" },
     { import = "lazyvim.plugins.extras.util.gitui" },
-    { import = "lazyvim.plugins.extras.util.project" },
 
     -- import/override with your plugins
     { import = "plugins" },
